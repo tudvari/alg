6b7c33dd5622f70ecad55feb1f54c6bdd4952220
Tibor Udvari <tudvari@tudvari.com>
2018-12-28 12:51:36 +0100

    remove unused logger

--- src/main/java/com/tudvari/tree/binary/InorderTraversal.java
+++ src/main/java/com/tudvari/tree/binary/InorderTraversal.java
@@ -2,12 +2,9 @@ package com.tudvari.tree.binary;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class InorderTraversal
 {
-    private final static Logger LOGGER = Logger.getLogger(InorderTraversal.class.getName());
-
     public static List traverse(Node root, List items)
     {
         if (root == null)
